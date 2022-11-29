1fc3100a8c1c2389022908f19275b63bbeb6d35f
Bobby Galli <bobbyg603@gmail.com>
2022-11-29 09:52:52 -0500

    Initial Commit

---
+++ MacSwiftCrasher/MacSwiftCrasherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacSwiftCrasherApp.swift
+//  MacSwiftCrasher
+//
+//  Created by Robert Galli on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct MacSwiftCrasherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
